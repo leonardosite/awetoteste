b5162b9c08a7f668231ac7a7f0af355fdfc44742
elysian-place <153707210+leonardosite@users.noreply.github.com>
2024-03-14 01:15:18 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,39 +1,16 @@
+//SISTEMA LUPA
 let box = document.querySelector('.fundo');
 let busca = document.querySelector('.lupa');
-
-
-box.addEventListener('mouseover', ()=>{
+let closee = document.querySelector('.fechar');
+busca.addEventListener('click', ()=>{
 box.classList.add('abrir');
 });
-
-box.addEventListener('mouseout', ()=>{
-  box.classList.remove('abrir');
-  });
-
-
-
-
-
-
-
-
-
-// ATIVAÇÃO DO MENU MOBILE
-const fundomenu= document.querySelector('.fundo-menu');
-const menuclique = document.querySelector('.clique-menu');
-const menufechar = document.querySelector('.fechar-menu');
-
-menuclique.addEventListener('click', ()=>{
-  fundomenu.classList.add("ativar-menu")
-})
-
-menufechar.addEventListener('click', ()=>{
-  fundomenu.classList.remove("ativar-menu")
+closee.addEventListener('click', ()=>{
+box.classList.remove('abrir')
 })
 
 
-
-// ATIVAÇÃO DO CARROSSEL
+//CARROSSEL
 var slideIndex = 0;
 showSlides();
 
@@ -54,12 +31,112 @@ function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-function addcarro(){
-  const titulocarro = document.querySelector(".titulo-produto-carrinho");
-  const titulobloco = document.querySelector(".titulo-produto")
-  
-  document.getElementById('titulo-produto-carrinho').innerHTML = 'titulobloco'
-}
+
+
+
+
+
+const fundo1 = document.querySelector('.fundo-menu1');
+const clique1 = document.querySelector('.clique-menu1');
+const fechar1 = document.querySelector('.menu1-fechar');
+
+const fundo2 = document.querySelector('.fundo-menu2');
+const clique2 = document.querySelector('.clique-menu2');
+const fechar2 = document.querySelector('.menu2-fechar');
+
+const fundo3 = document.querySelector('.fundo-menu3');
+const clique3 = document.querySelector('.clique-menu3');
+const fechar3 = document.querySelector('.menu3-fechar');
+
+
+const fundo4 = document.querySelector('.fundo-menu4');
+const clique4 = document.querySelector('.clique-menu4');
+const fechar4 = document.querySelector('.menu4-fechar');
+
+const fundo5 = document.querySelector('.fundo-menu5');
+const clique5 = document.querySelector('.clique-menu5');
+const fechar5 = document.querySelector('.menu5-fechar');
+
+const fundo6 = document.querySelector('.fundo-menu6');
+const clique6 = document.querySelector('.clique-menu6');
+const fechar6 = document.querySelector('.menu6-fechar');
+
+clique1.addEventListener('click', ()=>{
+  fundo1.classList.add('ativar-menu1');
+})
+
+fechar1.addEventListener('click',()=>{
+  fundo1.classList.remove('ativar-menu1')
+})
+
+clique2.addEventListener('click', ()=>{
+  fundo2.classList.add('ativar-menu2');
+})
+
+fechar2.addEventListener('click',()=>{
+  fundo2.classList.remove('ativar-menu2');
+})
+
+clique3.addEventListener('click', ()=>{
+  fundo3.classList.add('ativar-menu3');
+})
+
+fechar3.addEventListener('click',()=>{
+  fundo3.classList.remove('ativar-menu3');
+})
+
+clique4.addEventListener('click', ()=>{
+  fundo4.classList.add('ativar-menu4');
+})
+
+fechar4.addEventListener('click',()=>{
+  fundo4.classList.remove('ativar-menu4');
+})
+
+clique5.addEventListener('click', ()=>{
+  fundo5.classList.add('ativar-menu5');
+})
+
+fechar5.addEventListener('click',()=>{
+  fundo5.classList.remove('ativar-menu5');
+})
+
+clique6.addEventListener('click', ()=>{
+  fundo6.classList.add('ativar-menu6');
+})
+
+fechar6.addEventListener('click',()=>{
+  fundo6.classList.remove('ativar-menu6');
+})
+
+
+
+
+
+//SISTEMA MENU MOBILE
+const fundomenu= document.querySelector('.fundo-menu');
+const menuclique = document.querySelector('.clique-menu');
+const menufechar = document.querySelector('.fechar-menu');
+
+menuclique.addEventListener('click', ()=>{
+  fundomenu.classList.add("ativar-menu")
+})
+
+menufechar.addEventListener('click', ()=>{
+  fundomenu.classList.remove("ativar-menu")
+})
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -113,10 +190,3 @@ for (let i = 0; i < produtoinputmob.length; i++){
     }
 
 })
-
-
-
-
-
-
-
